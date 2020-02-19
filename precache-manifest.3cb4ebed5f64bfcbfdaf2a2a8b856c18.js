0baa57339d99bcccc464063719859d0d17a20d25
unknown <jacob.e.england@gmail.com>
2020-02-19 17:59:48 -0600

    Updates

---
+++ precache-manifest.3cb4ebed5f64bfcbfdaf2a2a8b856c18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8febb5ed870d40e974b9f9f8a1e8e09e",
+    "url": "/nasa-photo-of-the-day/index.html"
+  },
+  {
+    "revision": "a40d899268c92813bb5c",
+    "url": "/nasa-photo-of-the-day/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "a9956d69236b4ad28a98",
+    "url": "/nasa-photo-of-the-day/static/js/2.5e9296c5.chunk.js"
+  },
+  {
+    "revision": "a40d899268c92813bb5c",
+    "url": "/nasa-photo-of-the-day/static/js/main.273a68d2.chunk.js"
+  },
+  {
+    "revision": "350a0b8fbf7e20aef48a",
+    "url": "/nasa-photo-of-the-day/static/js/runtime~main.03586219.js"
+  }
+]);
--- src/PhotoContainer/Photo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Photo = (props) => {
-    return(
-        <img src={props.src} alt={props.title} title={props.title} />
-    )
-}
-
-export default Photo;
